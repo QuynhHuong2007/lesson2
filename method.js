0381d4011f804b59dc0c9cf3ee0ff3ae4f8a0bbf
Quynh Huong <nguyenquynhhuongpvt@gmail.com>
2023-10-05 20:01:30 +0700

    ko loi nhan

---
+++ method.js
@@ -0,0 +1,50 @@
+let x=1
+
+while (x<=10) {
+    console.log(x++)
+}
+
+for(let i=0; i<=100; i++){
+    console.log(`$`)
+}
+
+
+lfor (let i=0; i==0;i++){
+    let userInput=prompt("Hay")
+}
+if (userInput==="Hei"){
+
+}
+
+let arr=[1,1,1]
+
+let newArr=arr.map(
+    function a(num){
+        return num * 3
+    }
+)
+console.log(newArr)
+
+----------------------
+let o={
+    name:"vo thi thuy",
+    age:70
+}
+
+arr.push(o)
+
+console.log(arr)
+
+//object
+//CRUD
+
+let a={
+    name:"anno",
+    age:10;
+    trangthai:"thichdabanh",
+}
+//READ
+//console.log(o)
+
+//update
+o.age=70
